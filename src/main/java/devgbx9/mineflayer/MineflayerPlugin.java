c3d2c079a2e76b6220272d1e557f8ca5224c9b09
DevGBX9 <devgbx9@users.noreply.github.com>
2026-07-04 10:21:12 -0700

    add /mineflayer test command

--- src/main/java/devgbx9/mineflayer/MineflayerPlugin.java
+++ src/main/java/devgbx9/mineflayer/MineflayerPlugin.java
@@ -1,16 +1,29 @@
 package devgbx9.mineflayer;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MineflayerPlugin extends JavaPlugin {
+public class MineflayerPlugin extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
+        getCommand("mineflayer").setExecutor(this);
         getLogger().info(getName() + " v" + getPluginMeta().getVersion() + " enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info(getName() + " disabled!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length > 0 && args[0].equalsIgnoreCase("test")) {
+            sender.sendMessage("§aI'm working!");
+            return true;
+        }
+        return false;
     }
 }
